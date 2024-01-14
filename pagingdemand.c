363c7f6626ab70bdfad9b361bf91e8b218979b34
XMag27 <xmagalla@espol.edu.ec>
2024-01-14 09:53:39 -0500

    Cosntantes

--- pagingdemand.c
+++ pagingdemand.c
@@ -1,7 +1,11 @@
-//Import Default Libraries
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
+#define PAGE_TABLE_ENTRIES 256
+#define PAGE_SIZE 256
+#define FRAME_COUNT 256
+#define FRAME_SIZE 256
+#define PHYSICAL_MEMORY_SIZE FRAME_COUNT * FRAME_SIZE
 

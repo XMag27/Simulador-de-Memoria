46387325553a00fc8973bc72b9c2e9716ec06b29
XMag27 <xmagalla@espol.edu.ec>
2024-01-16 13:24:06 -0500

    Cambios y memoria fallando

--- pagingdemand.c
+++ pagingdemand.c
@@ -10,10 +10,17 @@
 #define PHYSICAL_MEMORY_SIZE FRAME_COUNT * FRAME_SIZE
 #define MAX_BUFFER_SIZE 10
 
+
+//Estructura para representar una entrada en la tabla de paginas
 typedef struct EntradaTablaPagina{
     uint8_t pfn;
-    char valido;
+    char valido; //Ver si existe o no un Page Fault
 } EntradaTablaPagina;
+
+//memoria fisica
+char memoria_fisica[PHYSICAL_MEMORY_SIZE];
+//tabla de paginas
+EntradaTablaPagina tabla_paginas[PAGE_TABLE_ENTRIES];
 
 int main (int argc, char *argv[]) {
 
@@ -56,6 +63,29 @@ int main (int argc, char *argv[]) {
         uint32_t offset = direccion_virtual & 0xFF;
         //Para obtener los 8 bits del VPN, se hace un corrimiento de 8 bits a la derecha y se vuelve a hacer una operación AND con 0xFF
         uint32_t vpn = (direccion_virtual >> 8) & 0xFF; 
-
+        //Si la entrada en la tabla de paginas no es valida, se carga la pagina desde el Backing Store
+        if (!(tabla_paginas[vpn].valido)) {
+            char *buffer_pagina = (char *) malloc(sizeof(char) * FRAME_SIZE);
+            //Preparar la pagina desde el Backing Store
+            if (fseek(BACKING_STORE, vpn * FRAME_SIZE, SEEK_SET) > 0) {
+                printf("Error al leer el archivo\n");
+                exit(1);
+            }
+            if (fread(buffer_pagina, 1, FRAME_SIZE, BACKING_STORE) < 0) {
+                printf("Error al leer el archivo\n");
+                exit(1);
+            }
+            //Cargar la pagina en la memoria fisica
+            memcpy(memoria_fisica + (tabla_paginas[vpn].pfn * FRAME_SIZE), buffer_pagina, FRAME_SIZE);
+            //Actualizar la tabla de paginas
+            tabla_paginas[vpn].pfn = tabla_paginas[vpn].pfn;
+            tabla_paginas[vpn].valido = 1;
+        }
+        //Obtener el valor de la direccion fisica
+        uint32_t direccion_fisica = (tabla_paginas[vpn].pfn * FRAME_SIZE) + offset;
+        //Obtener el valor de la direccion fisica
+        int8_t valor = memoria_fisica[direccion_fisica];
+        //Imprimir en la terminal en una sola linea
+        printf("Virtual address: %d Physical address: %d Value: %d\n", direccion_virtual, direccion_fisica, valor);
     }
 }
